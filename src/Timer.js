3fef60767febdbc5c486d60178d95bb59fe950e4
digvijay.rajpurohit <digvijay.rajpurohit@servicenow.com>
2020-01-30 15:18:58 +0530

    Finally completed

--- src/Timer.js
+++ src/Timer.js
@@ -1,17 +1,15 @@
 import React, { useState, useEffect } from "react"
 import './Timer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay, faSyncAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPlay, faSyncAlt, faStop } from '@fortawesome/free-solid-svg-icons'
+
+var id=null;
 
 function Timer(){
-    // const [hours, setHours] = useState(0);
-    // const [minutes, setMinutes] = useState(0);
-    // const [seconds, setSeconds] = useState(0);
-    // const [milliseconds, setMilliseconds] = useState(0);
     const [count, setCount] = useState(0);
     const [timer, setTimer] = useState(0);
     const [running, setRunning] = useState(false)
-    const [id, setId] = useState(null)
+
     function handleChange(event){
         const input=event.target.value;
         if(input=="")
@@ -21,39 +19,32 @@ function Timer(){
         }
     }
 
-    function check(i){
-        if(i<10)
-            return "0"+i;
-        return i;
-    }
-
     useEffect(()=>{
         setTimer(count*1000)
     },[count])
 
     useEffect(()=>{
         if(running)
-            startRun(id)
+            startRun()
         else
-            stopRun(id)
+            stopRun()
     },[running])
 
-    function startRun(id){
-        console.log("start timer")
+    function startRun(){
         id = setInterval(()=>{
             setTimer((timer)=>{
                 if(timer==0){
+                    setRunning(false)
                     clearInterval(id);
                     return 0;
                 }
                 return timer-10;
             })
         },10)
-        console.log(id)
+        return id;
     }
 
-    function stopRun(id){
-        console.log(id)
+    function stopRun(){
         clearInterval(id);
         setCount(timer/1000);
     }
@@ -61,6 +52,50 @@ function Timer(){
     function handleReset(){
         setCount(0)
     }
+
+    return(
+        <div className="timer-class">
+            <div className="timer-display-class">
+                {!running ?
+                <div>
+                    <div className="countdown-timer-notrunning-class">
+                        {check(getHours(timer))}:{check(getMinutes(timer))}:{check(getSeconds(timer))}:{check(getMilliseconds(timer))}
+                    </div>
+                    <div className="count-class">
+                        <input 
+                            type="text" 
+                            value={Math.floor(count)} 
+                            className="input-class" 
+                            onChange={handleChange}
+                        />
+                    </div>
+                </div>
+                :
+                <div 
+                    className="countdown-timer-running-class" 
+                    style={{color:  timer<=10000 ? "#ff3333" : "#17a2b8" }}
+                >
+                    {check(getHours(timer))}:{check(getMinutes(timer))}:{check(getSeconds(timer))}:{check(getMilliseconds(timer))}
+                </div>
+                }
+            </div>
+            <div className="button-class">
+                { !running?
+                <button className="play-class" onClick={()=>{if(count!=0)setRunning(true)}}
+                    >
+                    <FontAwesomeIcon icon={faPlay} />
+                </button>
+                :
+                <button className="stop-class" onClick={()=>setRunning(false)}>
+                    <FontAwesomeIcon icon={faStop} />
+                </button>
+                }
+                <button className="reset-class" onClick={handleReset}>
+                    <FontAwesomeIcon icon={faSyncAlt} />
+                </button>
+            </div>
+        </div>
+    )
 
     function getHours(timer){
         return Math.floor(timer/3600/1000);
@@ -80,32 +115,11 @@ function Timer(){
         return Math.floor((timer%1000)/10);
     }
 
-    return(
-        <div className="timer-class">
-            <div className="timer-display-class">
-                <div className="countdown-timer-class">
-                    {check(getHours(timer))}:{check(getMinutes(timer))}:{check(getSeconds(timer))}:{check(getMilliseconds(timer))}
-                </div>
-                <div className="count-class">
-                    <input 
-                        type="text" 
-                        value={count} 
-                        className="input-class" 
-                        onChange={handleChange}
-                    />
-                </div>
-            </div>
-            <div className="button-class">
-                <button className="play-class" onClick={()=>setRunning(run=>!run)}
-                    >
-                    <FontAwesomeIcon icon={faPlay} />
-                </button>
-                <button className="reset-class" onClick={handleReset}>
-                    <FontAwesomeIcon icon={faSyncAlt} />
-                </button>
-            </div>
-        </div>
-    )
+    function check(i){
+        if(i<10)
+            return "0"+i;
+        return i;
+    }
 }
 
 export default Timer;
